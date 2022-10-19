a68b7b059356f42faca6bcd35f56eaaf995013b8
YuYeongjin <svvo2002@naver.com>
2022-10-19 17:42:02 +0900

    20221019 spring 수업

---
+++ src/main/java/org/zerock/controller/lecture/p06jdbc/Controller23.java
@@ -0,0 +1,31 @@
+package org.zerock.controller.lecture.p06jdbc;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("ex23")
+public class Controller23 {
+	@Autowired
+	private DataSource dataSource;
+	
+	@RequestMapping("sub01")
+	public void method1() throws Exception {
+		String sql ="SELECT * FROM Customers";
+		try(Connection con = dataSource.getConnection();
+				Statement stmt= con.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);
+				){
+			if(rs.next()) {
+				System.out.println(rs.getString("customerName"));
+			} 
+		}
+	}
+}
