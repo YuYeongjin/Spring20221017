0ffcec32f228a4cd3cdb0a89e672d0cddca19c57
YuYeongjin <108460833+YuYeongjin@users.noreply.github.com>
2022-10-22 14:51:25 +0900

    20221022 spring

--- src/main/java/org/zerock/controller/lecture/p03forward/Controller14.java
+++ src/main/java/org/zerock/controller/lecture/p03forward/Controller14.java
@@ -1,10 +1,13 @@
 package org.zerock.controller.lecture.p03forward;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
 
 @Controller
 @RequestMapping("ex14")
@@ -16,7 +19,7 @@ public class Controller14 {
 	// 6 forward/redirect
 	
 	@RequestMapping("sub01")
-	public void method1(HttpServletRequest req, HttpServletResponse res) {
+	public void method1(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		System.out.println("메소드 1번 일함!");
 		String path = "/WEB-INF/views/ex14/sub01.jsp";
 		req.getRequestDispatcher(path).forward(req, res);
